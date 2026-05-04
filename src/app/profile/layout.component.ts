c5943d94b423c04f149b674a3d76bd0796ae43b2
Urjam Fals <jumarfal45@gmail.com>
2026-05-05 01:29:31 +0800

    Another commit with src/home and src/profile files

---
+++ src/app/profile/layout.component.ts
@@ -0,0 +1,4 @@
+import { Component } from '@angular/core';
+
+@Component({ selector: 'app-profile-layout', templateUrl: 'layout.component.htm;', standalone: false })
+export class LayoutComponent { }
